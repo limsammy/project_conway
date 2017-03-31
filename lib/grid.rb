df39a251fb3a0a427554a271a93c49118bc5579d
Sam Lim <limsammy1@gmail.com>
2017-03-31 11:41:50 -0600

    added pseudocode for fill_grid method

--- lib/grid.rb
+++ lib/grid.rb
@@ -7,4 +7,15 @@ class Grid
     @length = length
     @width = width
   end
+
+  def check_relative_cells
+# implement a way to render cells on the grid
+# before writing this method to check if neighbors
+# are alive or dead
+  end
+
+  def fill_grid
+# takes input of grid location to put an alive cell
+# fills rest of grid with dead cells
+  end
 end
